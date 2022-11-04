be87ea1c6f0dcdedd3e3616491ad10cbe882aac6
Levent Kalkavan <levent.kalkavan@hotmail.com>
2022-11-04 18:25:27 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Concrete
+{
+  public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-TSVO3IT;database=CoreBlogDb;integrated security=true");
+        }
+    }
+}
